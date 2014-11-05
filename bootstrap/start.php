2e5c60b1099a0de05153f892a2d96ee8dc8d29c4
DrZippie <antonio@antoniocortes.com>
2014-11-05 13:35:45 +0100

    Get Modules config parsing install-modules.conf

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -11,17 +11,28 @@ include_once 'extensions/vendors/autoload.php';
 class_alias('Ximdex\Runtime\App', 'App');
 
 
-App::setValue('XIMDEX_ROOT_PATH', dirname(dirname(__FILE__)))
-    ->setValue('devel', true);
+
+
+App::setValue('XIMDEX_ROOT_PATH', dirname(dirname(__FILE__))) ;
 
 
 // get Config
 $conf = require_once(App::getValue('XIMDEX_ROOT_PATH') . '/conf/config.php');
-
-
 foreach ($conf as $key => $value) {
     App::setValue($key, $value);
 }
 
+
+// read install-modules.conf
+$modulesConfString = file_get_contents( App::getValue('XIMDEX_ROOT_PATH') . '/conf/install-modules.conf' ) ;
+$matches = array();
+preg_match_all( '/define\(\'(.*)\',(.*)\);/iUs' , $modulesConfString, $matches );
+foreach( $matches[1] as $key => $value ) {
+    App::setValue( $value  , str_replace( '\'', '', $matches[2][$key ]));
+}
+
+// use config values
 define('DEFAULT_LOCALE', App::getValue('locale'));
 date_default_timezone_set(App::getValue('timezone'));
+
+
