d778f3d4c85a0a38e09e71f1f028709d520c745d
DrZippie <antonio@antoniocortes.com>
2014-11-05 13:15:15 +0100

    Sample Config and load values

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -15,3 +15,13 @@ App::setValue('XIMDEX_ROOT_PATH', dirname(dirname(__FILE__)))
     ->setValue('devel', true);
 
 
+// get Config
+$conf = require_once(App::getValue('XIMDEX_ROOT_PATH') . '/conf/config.php');
+
+
+foreach ($conf as $key => $value) {
+    App::setValue($key, $value);
+}
+
+define('DEFAULT_LOCALE', App::getValue('locale'));
+date_default_timezone_set(App::getValue('timezone'));
---
+++ conf/config.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// TODO: Generate this file in Install Process
+
+return array(
+
+    'db' => array(
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'port' => '3306',
+        'user' => 'ximdex',
+        'password' => 'ximdex',
+        'db' => 'ximdex',
+        'log' => false,
+
+    ),
+    'timezone' => 'Europe/London',
+    'locale' => 'en_US',
+) ;
